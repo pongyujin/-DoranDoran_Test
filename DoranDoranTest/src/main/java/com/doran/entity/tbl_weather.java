d201b370dd7e1252e766044c8cf583fbb9982c47
pongyujin <pongyujin@naver.com>
2024-10-14 16:18:09 +0900

    정유진/2024-10-14 오후 4:17/엔터티 생성,root-context 변경,pom.xml 변경,통계페이지 생성

---
+++ DoranDoranTest/src/main/java/com/doran/entity/tbl_weather.java
@@ -0,0 +1,23 @@
+package com.doran.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class tbl_weather {
+    private int wNum; // 기상 번호
+    private String wDate; // 기상일
+    private String wTime; // 기상 시간
+    private double wTemp; // 온도
+    private double wWindSpeed; // 풍속
+    private double wWaveHeight; // 파고
+    private double wSeaTemp; // 해수 온도
+    private String wRegion; // 기상 지역
+}
