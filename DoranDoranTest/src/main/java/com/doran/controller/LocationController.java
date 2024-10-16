2d65256e72294231d5f27d65e4b3742ef6557099
olilvado <jitejiae@gmail.com>
2024-10-16 15:07:17 +0900

    박소희, 지도js 추가 및 수정, 16일 15:07

--- DoranDoranTest/src/main/java/com/doran/controller/LocationController.java
+++ DoranDoranTest/src/main/java/com/doran/controller/LocationController.java
@@ -11,6 +11,7 @@ public class LocationController {
 	@GetMapping("/location")
 	public String getLocationInfo(@RequestParam double latitude, @RequestParam double longitude) {
 		RestTemplate restTemplate = new RestTemplate();
+		
 		String apiKey = "AIzaSyAW9QwdMPgIykOFaLdCX5ZJTQOED8FVLfg"; // API 키를 설정하세요.
 		String url = String.format("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s&language=ko&key=%s",
 				latitude, longitude, apiKey);
