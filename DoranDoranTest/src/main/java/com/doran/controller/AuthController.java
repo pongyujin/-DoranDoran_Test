ce32d9a4972569c3f02cfb1cee686e4d2384f8d1
olilvado <jitejiae@gmail.com>
2024-10-16 17:04:06 +0900

    박소희, 권한기능구현, 16일 17:03

--- DoranDoranTest/src/main/java/com/doran/controller/AuthController.java
+++ DoranDoranTest/src/main/java/com/doran/controller/AuthController.java
@@ -1,22 +1,83 @@
 package com.doran.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.doran.entity.tbl_group;
+import com.doran.entity.tbl_ship;
 import com.doran.mapper.AuthMapper;
-import com.doran.mapper.tbl_shipstatMapper;
 
+@RequestMapping("/group")
 @Controller
 public class AuthController {
 
 	@Autowired
 	private AuthMapper authMapper;
 
-	@GetMapping("")
-	public String auth() {
-		return ""; 
+	// 1. 선박번호로 그룹 멤버 리스트 가져오기
+	@GetMapping("/list")
+	public List<tbl_group> groupList(String siCode) {
+
+		List<tbl_group> groupList = authMapper.groupList(siCode);
+		return groupList;
 	}
 
+	// 2. 그룹 초대
+	@PostMapping("/invite")
+	public void invite(@RequestBody tbl_group tbl_group) {
+
+		tbl_group.setAuthNum(0);
+		authMapper.invite(tbl_group);
+	}
+
+	// 3. 그룹 권한 수정(권한 부여)
+	@PutMapping("/update")
+	public String update(@RequestBody tbl_group tbl_group, tbl_ship tbl_ship, RedirectAttributes rttr) {
+
+		if (topAuth(tbl_ship)) {
+
+			authMapper.update(tbl_group);
+		} else {
+
+			rttr.addFlashAttribute("msgType", "실패");
+			rttr.addFlashAttribute("msg", "회원 관리 권한이 없습니다.");
+		}
+		return "redirect:/";
+	}
+
+	// 4. 관리자 확인(로그인한 아이디가 선박 등록인과 일치하는지)
+	public boolean topAuth(tbl_ship tbl_ship) {
+
+		tbl_ship check = authMapper.topAuth(tbl_ship);
+
+		if (check != null) {
+			return true;
+		} else
+			return false;
+	}
+
+	// 5. 회원 삭제
+	@DeleteMapping("/delete")
+	public void delete(@RequestBody tbl_group tbl_group, tbl_ship tbl_ship, RedirectAttributes rttr) {
+
+		if (topAuth(tbl_ship)) {
+
+			authMapper.delete(tbl_group);
+		} else {
+
+			rttr.addFlashAttribute("msgType", "실패");
+			rttr.addFlashAttribute("msg", "회원 삭제 권한이 없습니다.");
+		}
+	}
 }
--- DoranDoranTest/src/main/java/com/doran/mapper/AuthMapper.java
+++ DoranDoranTest/src/main/java/com/doran/mapper/AuthMapper.java
@@ -1,10 +1,24 @@
 package com.doran.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.doran.entity.tbl_group;
+import com.doran.entity.tbl_ship;
 
 @Mapper
 public interface AuthMapper {
 
-	
+	// 1. 그룹 멤버 리스트 전체 불러오기
+	public List<tbl_group> groupList(String siCode);
+	// 2. 그룹 초대
+	public void invite(tbl_group tbl_group);
+	// 3. 권한 수정
+	public void update(tbl_group tbl_group);
+	// 4. 선박 등록자 확인
+	public tbl_ship topAuth(tbl_ship tbl_ship);
+	// 5. 회원 삭제
+	public void delete(tbl_group tbl_group);
 	
 }
