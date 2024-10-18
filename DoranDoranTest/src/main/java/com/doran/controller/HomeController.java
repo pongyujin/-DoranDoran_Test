5a3fcba8998b26beea8c5062353fd15d860a0fbb
pongyujin <pongyujin@naver.com>
2024-10-18 15:13:57 +0900

    정유진/한번만요

--- DoranDoranTest/src/main/java/com/doran/controller/HomeController.java
+++ DoranDoranTest/src/main/java/com/doran/controller/HomeController.java
@@ -1,14 +1,8 @@
 package com.doran.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -27,6 +21,6 @@ public class HomeController {
 	@GetMapping("/main")
 	public String showMainPage() {
 		return "Main";
-	
-}
+
+	}
 }
