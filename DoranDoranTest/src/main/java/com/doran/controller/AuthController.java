2f3807194d6fe352b875fd3964b2e7561d459762
pongyujin <pongyujin@naver.com>
2024-10-18 15:13:57 +0900

    정유진/한번만요

--- DoranDoranTest/src/main/java/com/doran/controller/AuthController.java
+++ DoranDoranTest/src/main/java/com/doran/controller/AuthController.java
@@ -2,8 +2,6 @@ package com.doran.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.doran.entity.ShipGroup;
 import com.doran.entity.Ship;
+import com.doran.entity.ShipGroup;
 import com.doran.mapper.AuthMapper;
 
 // 그룹의 권한을 관리하고 수정/삭제하는 controller
