791bf1db7ecce84c74e20c2c7d79081b2ef6f775
smhrd12 <smhrd12@192.168.219.109>
2024-10-30 15:15:59 +0900

    ㅇ

--- DoranDoranTest/src/main/java/com/doran/entity/Camera.java
+++ DoranDoranTest/src/main/java/com/doran/entity/Camera.java
@@ -16,7 +16,7 @@ public class Camera {
     private int imgNum; // 이미지 번호
     private String siCode; // 선박 코드
     private String obsName; // 장애물 이름
-    private String obsImg; // 장애물 이미지
+    private byte[] obsImg; // 장애물 이미지 (BLOB로 저장할 바이트 배열)
     private String createdAt; // 등록 시간
     private int sailNum; // 항해 번호
 }
