e32581f88b276c1ebd0354e344811f32ebe04abe
smhrd11 <smhrd11@192.168.219.120>
2024-10-30 14:32:45 +0900

    10-30 배터리 전압체크

--- DoranDoranTest/src/main/java/com/doran/controller/HwBatteryController.java
+++ DoranDoranTest/src/main/java/com/doran/controller/HwBatteryController.java
@@ -4,16 +4,17 @@ import org.springframework.web.bind.annotation.*;
 import org.json.JSONObject;
 
 @RestController
-@RequestMapping("/receive_voltage")
-public class HwMotorController {
+@RequestMapping("/controller")
+public class HwBatteryController {
 
-    @PostMapping
+    @PostMapping("/receive_voltage")
     public String receiveVoltage(@RequestBody String jsonData) {
         try {
+            // JSON 파싱
             JSONObject json = new JSONObject(jsonData);
             double voltage = json.getDouble("voltage");
 
-            // 받은 전압 데이터 출력
+            // 전압 데이터 출력
             System.out.println("Received voltage: " + voltage + " V");
             return "Voltage received successfully";
 
